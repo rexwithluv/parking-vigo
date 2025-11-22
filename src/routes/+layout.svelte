ab78d309a8c774cbfbd5cc50e18685eadc8831d4
rexwithluv <aaronaax@gmail.com>
2025-11-22 15:22:57 +0100

    feat: add favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import faviconBlack from '$lib/assets/favicon-black.png';
+	import faviconWhite from '$lib/assets/favicon-white.png';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href={faviconBlack} type="image/png" media="(prefers-color-scheme: light)" />
+
+	<link rel="icon" href={faviconWhite} type="image/png" media="(prefers-color-scheme: dark)" />
 </svelte:head>
 
 {@render children()}
